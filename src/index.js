3445dbc2d8aed4908f6c5b9dc7cf5093dc7179e6
Zrazhevskii <vbn-134@yandex.ru>
2024-05-24 11:14:25 +0300

    первый слайдер

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import './index.html';
-import './blocks/blocks'
-import './index.scss'
+import './blocks/blocks';
+import './index.scss';
 // import './index.css'
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    freeMode: true,
+    loop: true,
+});
