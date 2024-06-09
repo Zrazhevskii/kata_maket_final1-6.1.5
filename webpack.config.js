30cda7209e5051bcbd31ba00623ef8218c4f16bf
Zrazhevskii <vbn-134@yandex.ru>
2024-06-09 16:02:38 +0300

    замена div на интерактивы

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    // clean: true,
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[name][ext]'
   },
