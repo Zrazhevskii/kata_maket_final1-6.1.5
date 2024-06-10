db3af0517538bddf13e0713b3e38422e66fc4e8c
Zrazhevskii <vbn-134@yandex.ru>
2024-06-10 11:32:13 +0300

    коррекция БЭМ content__btn

--- src/blocks/main/main_content/main_content.js
+++ src/blocks/main/main_content/main_content.js
@@ -1,29 +1,29 @@
 /* eslint-env es6 */
 /* eslint-disable no-console */
 
-const contentBtn = document.querySelector('.main__content-ownards-btn');
-const moreText = document.querySelector('.main__content-more-text');
+const contentBtn = document.querySelector('.content__btn');
+const moreText = document.querySelector('.content__more__text');
 const contentTextHidden = document.querySelector('.content__text_hidden');
-const moreHidden = document.querySelector('.more--hidden');
+const moreHidden = document.querySelector('.content__more__hidden');
 const mainContentTextBox = document.querySelector('.content__text__box');
 
 contentBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
 
-  if (contentBtn.classList.contains('content-btn')) {
-    contentBtn.classList.remove('content-btn');
+  if (contentBtn.classList.contains('content__btn_control')) {
+    contentBtn.classList.remove('content__btn_control');
 
     contentTextHidden.classList.remove('content__text_hidden');
-    moreText.classList.remove('main__content-more-text');
-    moreHidden.classList.remove('more--hidden');
+    moreText.classList.remove('content__more__text');
+    moreHidden.classList.remove('content__more__hidden');
 
-    contentBtn.classList.add('content-btn--hide');
+    contentBtn.classList.add('content__btn_hide');
   } else {
-    contentBtn.classList.remove('content-btn--hide');
+    contentBtn.classList.remove('content__btn_hide');
 
     contentTextHidden.classList.add('content__text_hidden');
-    moreText.classList.add('main__content-more-text');
-    moreHidden.classList.add('more--hidden');
-    contentBtn.classList.add('content-btn');
+    moreText.classList.add('content__more__text');
+    moreHidden.classList.add('content__more__hidden');
+    contentBtn.classList.add('content__btn_control');
   }
 });
