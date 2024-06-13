1fb9f688384abb8280dc4ff2c6cb6538ee219249
Zrazhevskii <vbn-134@yandex.ru>
2024-06-13 08:55:13 +0300

    корректировка description

--- src/blocks/main/main__description/main__description.js
+++ src/blocks/main/main__description/main__description.js
@@ -10,8 +10,21 @@ mainItem.addEventListener('click', (evt) => {
   mainItemLink.forEach((item) => {
     if (item.classList.contains('servises__link_active')) {
       item.classList.remove('servises__link_active');
+      if (item === mainItemLink[0]) {
+        mainItemLink[0].classList.remove('servises__link_background');
+        mainItemLink[0].classList.add('servises__link_gradient');
+      } else if (
+        mainItemLink[0].classList.contains('servises__link_gradient')
+      ) {
+        return;
+      }
     }
   });
+
+  if (evt.target === mainItemLink[0]) {
+    mainItemLink[0].classList.add('servises__link_background');
+    mainItemLink[0].classList.remove('servises__link_gradient');
+  }
 
   evt.target.classList.add('servises__link_active');
 });
